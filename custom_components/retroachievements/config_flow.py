08633efdd05b302d4b20d2a646a8fa41d799262b
kllngtme <kllngtme82@yahoo.com>
2025-08-30 23:08:11 -0400

    Update config_flow.py

--- custom_components/retroachievements/config_flow.py
+++ custom_components/retroachievements/config_flow.py
@@ -1,29 +1,20 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN, CONF_USERNAME, CONF_API_KEY
+from .const import DOMAIN, CONF_USERNAME, CONF_API_KEY, CONF_NUM_GAMES
 
 DATA_SCHEMA = vol.Schema({
     vol.Required(CONF_USERNAME): str,
     vol.Required(CONF_API_KEY): str,
+    vol.Optional(CONF_NUM_GAMES, default=3): vol.All(int, vol.Range(min=1, max=15)),
 })
 
 class RetroAchievementsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 1
-
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="RetroAchievements", data=user_input)
-        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
 
-    @callback
-    def async_get_options_flow(config_entry):
-        return RetroAchievementsOptionsFlow(config_entry)
-
-
-class RetroAchievementsOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        return self.async_show_form(step_id="init")
+        return self.async_show_form(
+            step_id="user",
+            data_schema=DATA_SCHEMA,
+            description_placeholders={"num_games": "Number of Games to Monitor (including Active/Last Played)"}
+        )
